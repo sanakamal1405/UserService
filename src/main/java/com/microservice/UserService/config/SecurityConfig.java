e53532b7012b6c8f474d20b9c8d39b146123741e
Sana Kamal <sanakamal1405@gmail.com>
2024-06-03 13:51:27 +0530

    Postman Client added

--- src/main/java/com/microservice/UserService/config/SecurityConfig.java
+++ src/main/java/com/microservice/UserService/config/SecurityConfig.java
@@ -95,15 +95,14 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
         @Bean
         public RegisteredClientRepository registeredClientRepository() {
             RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                    .clientId("oidc-client")
-                    .clientSecret("{noop}secret")
+                    .clientId("scalar")
+                    .clientSecret("{noop}scalar")
                     .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                     .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                     .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                    .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                    .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                    .scope(OidcScopes.OPENID)
-                    .scope(OidcScopes.PROFILE)
+                    .redirectUri("https://oauth.pstmn.io/v1/callback")
+                    .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                    .scope("ADMIN")
                     .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                     .build();
 
