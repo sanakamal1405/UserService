ea768b471dfafd8bb49d5c3db1b0f585f1505bec
Sana Kamal <sanakamal1405@gmail.com>
2024-06-03 15:18:28 +0530

    Added Bcrypt Password Encoder

---
+++ src/main/java/com/microservice/UserService/config/BcryptConfig.java
@@ -0,0 +1,20 @@
+package com.microservice.UserService.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BcryptConfig {
+
+    @Bean
+    public BCryptPasswordEncoder getBcryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
+
--- src/main/java/com/microservice/UserService/config/SecurityConfig.java
+++ src/main/java/com/microservice/UserService/config/SecurityConfig.java
@@ -83,9 +83,9 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
         @Bean
         public UserDetailsService userDetailsService() {
-            UserDetails userDetails = User.withDefaultPasswordEncoder()
+            UserDetails userDetails = User.builder()
                     .username("user")
-                    .password("password")
+                    .password("$2a$12$hWQDpKd3LQL5OajT43uFV.BbaoL1y5kJKCt3EjWfSiughE1HMVxhm")
                     .roles("USER")
                     .build();
 
@@ -95,15 +95,14 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
         @Bean
         public RegisteredClientRepository registeredClientRepository() {
             RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                    .clientId("oidc-client")
-                    .clientSecret("{noop}secret")
+                    .clientId("scalar")
+                    .clientSecret("$2a$12$Cxj4axfKcC5odCJmhQg6XuEQQ5Szd28FYyAbAJNSYUV6IpFeqNHXq")
                     .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                     .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                     .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                    .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                    .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                    .scope(OidcScopes.OPENID)
-                    .scope(OidcScopes.PROFILE)
+                    .redirectUri("https://oauth.pstmn.io/v1/callback")
+                    .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                    .scope("ADMIN")
                     .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                     .build();
 
